6dbfb1310aaff7eff84f919fc1cf096d12c941a6
Miquel Vidal <miquelvidalcortes@gmail.com>
2022-04-21 18:48:41 +0200

    json funcionallllllll

--- Personal/assets/js/events_per_tipus.js
+++ Personal/assets/js/events_per_tipus.js
@@ -1,14 +1,14 @@
 
 
-function cargarDatos(){
-    
+function cargarDatos(obj){
     var xmlhttp = new XMLHttpRequest();
     var url = "assets/js/events.json";
-
+    console.log(obj);
+    console.log(obj.name);
     xmlhttp.onreadystatechange = function(){
         if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
             dades = JSON.parse(xmlhttp.responseText);
-            dataVisualizar(dades);
+            dataVisualizar(dades, obj.name);
         }
     };
 
@@ -16,16 +16,14 @@ function cargarDatos(){
     xmlhttp.send();
 }
 
-function dataVisualizar(data){
+function dataVisualizar(data, name){
     let date = new Date();
     var actualDate = date.toISOString();
 
     for (let index = 0; index < data.length; index++) {
-            if(data[index].startDate > actualDate){
+            if(name == "fira"){
                 visualitzarEvent(data[index]);
-            }else{
-                console.log("Hola");
-            }     
+            }
     }
 }
 
@@ -33,13 +31,19 @@ function visualitzarEvent(data){
     const contenidor = document.createElement("div");
     const contenidor2 = document.createElement("div");
     const contenidor3 = document.createElement("div");
+    const contenidor4 = document.createElement("div");
     const titol = document.createElement("h4");
     const link1 = document.createElement("a");
     const link2 = document.createElement("a");
     const text = document.createElement("p");
-    contenidor.className = "col-lg-4 col-md-6 d-flex align-items-stretch mt-4";
+    const tit = document.createElement("h1");
+    contenidor.className = "col-lg-4 col-md-6 d-flex align-items-stretch ";
     contenidor2.className = "shadow icon-box";
     contenidor3.className = "icon";
+    logoFira = document.createElement('img');
+    logoFira.src = "assets/svg/fira.svg"
+    logoFira.class = "image.fluid";
+    contenidor3.appendChild(logoFira);
     link1.innerHTML = data.name;
     text.innerHTML = data.description;
     link2.href = "#";
@@ -51,8 +55,10 @@ function visualitzarEvent(data){
     contenidor2.appendChild(text);
     contenidor2.appendChild(link2);
     contenidor.appendChild(contenidor2);
-    eventsProxims.appendChild(contenidor);
+    tit.innerHTML = "Fires";
+    titolSeccio.appendChild(tit);
+    eventsProximsPerTipus.appendChild(contenidor);
     
 }
 
-cargarDatos();
+
