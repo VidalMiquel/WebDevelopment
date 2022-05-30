2a7d35a7e2bbcc0d40dafe7d20ca87100d0e4610
Miquel Vidal <miquelvidalcortes@gmail.com>
2022-05-30 18:53:21 +0200

    fixed errors

--- Personal/assets/js/jsonextern.js
+++ Personal/assets/js/jsonextern.js
@@ -3,9 +3,26 @@ NOM DEL FITXER: jsonextern.js
 FUNCIONALITAT: obetenir dades del json extern. 
 ON TROBAM AQUESTA FUNCINALITAT: event.html
 */
+let datos_json = [];
+
+window.onload = async function () {
+
+    await cargarDatos(); //Carrega les dades de la pagina web
+    dataVisualizar(datos_json);
+
+};
+
+async function cargarDatos() {
+
+    let respuesta = await fetch("https://gastronomiaesp.000webhostapp.com/JSON/gastronomia.json");
+    let datos = await respuesta.json();
+    datos_json = datos;
+
+}
 
 
-function cargarDatos() {
+
+/* function cargarDatos() {
     var xmlhttp = new XMLHttpRequest();
     //Fitxer a obtenir.
     var url = "https://gastronomiaesp.000webhostapp.com/JSON/gastronomia.json";
@@ -15,25 +32,28 @@ function cargarDatos() {
             //Parsejam el resultat de la crida per poder tractar les dades.
             dades = JSON.parse(xmlhttp.responseText);
             //Passam per paràmetre el contingut del fitxer per poder integrar-ho dins la pràctica.
-            dataVisualizar(dades);
             console.log(dades);
+            dataVisualizar(dades);
         }
     };
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
-}
+} */
 
 //Visualitzar plat del json extern.
 function dataVisualizar(dades) {
+    
     for (let index = 0; index < 6; index++) {
         //Obtenim plat a visualitzar.
-        let numero = generateRandomInt(0, 103);
+        const numero = generateRandomInt(0, 103);
+        //console.log(numero);
+        //console.log(dades.gastronomia[numero])
         //console.log("https://gastronomiaesp.000webhostapp.com/" + dades.gastronomia[numero].image[0].name.substr(3));
-        console.log(dades.gastronomia[numero]);
         const nomPlat = document.createElement("h1");
-        console.log(dades.gastronomia[numero].name);
+        //console.log(dades.gastronomia[numero].name);
         nomPlat.innerHTML = dades.gastronomia[numero].name;
         nomPlat.id = "nomPlat";
+        //console.log(nomPlat);
         const imatgePlat = document.createElement("img");
         imatgePlat.src = "https://gastronomiaesp.000webhostapp.com/" + dades.gastronomia[numero].image[0].name.substr(3);
         imatgePlat.id = "fotoPlat";
@@ -51,13 +71,8 @@ function dataVisualizar(dades) {
         element.appendChild(localitzacioPlat);
         element.appendChild(imatgePlat);
         caruselPlats.appendChild(element);
+
     }
-
-
-
-
-
-
 
 }
 
@@ -65,5 +80,3 @@ function dataVisualizar(dades) {
 function generateRandomInt(min, max) {
     return Math.floor((Math.random() * (max - min)) + min);
 }
-
-cargarDatos();
