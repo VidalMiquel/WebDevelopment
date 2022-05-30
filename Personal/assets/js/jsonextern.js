deb1f1904ee380857d8839b6820bb8dcdae25821
Miquel Vidal <miquelvidalcortes@gmail.com>
2022-05-30 16:53:15 +0200

    solucionat ?

--- Personal/assets/js/jsonextern.js
+++ Personal/assets/js/jsonextern.js
@@ -22,35 +22,30 @@ function cargarDatos() {
 function dataVisualizar(dades) {
 
     for (let index = 0; index < 6; index++) {
-        //let numero = generateRandomInt(0, dades.gastronomia.length());
-       // let numero = generateRandomInt(0, 70);
-       console.log("https://gastronomiaesp.000webhostapp.com/" + dades.gastronomia[38].image[0].name);
-        if ("https://gastronomiaesp.000webhostapp.com/" + dades.gastronomia[38].image[0].name) {
-            const nomPlat = document.createElement("h1");
-            nomPlat.innerHTML = dades.gastronomia[38].name;
-            nomPlat.id = "nomPlat";
-            const imatgePlat = document.createElement("img");
-            imatgePlat.src = "https://gastronomiaesp.000webhostapp.com/" + dades.gastronomia[38].image[0].name;
-            imatgePlat.id = "fotoPlat";
-            imatgePlat.className = "img-fluid";
-            const localitzacioPlat = document.createElement("h2");
-            localitzacioPlat.id = "localitzacioPlat";
-            localitzacioPlat.innerHTML = dades.gastronomia[38].datosextra.comunidadAutonoma;
-            const element = document.createElement("div");
-            if (index == 0) {
-                element.className = "carousel-item active";
-            } else {
-                element.className = "carousel-item";
-            }
-            element.appendChild(nomPlat);
-            element.appendChild(localitzacioPlat);
-            element.appendChild(imatgePlat);
-            caruselPlats.appendChild(element);
-        } else {
-            index--;
-            console.log("no te foto");
-        }
+        let max = dades.gastronomia.length;
+        let numero = generateRandomInt(0, max);
+        console.log("https://gastronomiaesp.000webhostapp.com/" + dades.gastronomia[numero].image[0].name);
 
+        const nomPlat = document.createElement("h1");
+        nomPlat.innerHTML = dades.gastronomia[numero].name;
+        nomPlat.id = "nomPlat";
+        const imatgePlat = document.createElement("img");
+        imatgePlat.src = "https://gastronomiaesp.000webhostapp.com/" + dades.gastronomia[numero].image[0].name;
+        imatgePlat.id = "fotoPlat";
+        imatgePlat.className = "img-fluid";
+        const localitzacioPlat = document.createElement("h2");
+        localitzacioPlat.id = "localitzacioPlat";
+        localitzacioPlat.innerHTML = dades.gastronomia[numero].datosextra.comunidadAutonoma;
+        const element = document.createElement("div");
+        if (index == 0) {
+            element.className = "carousel-item active";
+        } else {
+            element.className = "carousel-item";
+        }
+        element.appendChild(nomPlat);
+        element.appendChild(localitzacioPlat);
+        element.appendChild(imatgePlat);
+        caruselPlats.appendChild(element);
     }
 
 
